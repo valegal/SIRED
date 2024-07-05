f2613be21e614cd069de81b29ee698c02e71f2fd
Valentina Galeano <99009069+valegal@users.noreply.github.com>
2024-07-05 16:16:16 -0500

    Generate table Variables

--- frontend/src/components/datos/TablaMediciones.jsx
+++ frontend/src/components/datos/TablaMediciones.jsx
@@ -8,15 +8,10 @@ const TablaMediciones = () => {
     const [conteo, setConteo] = useState({ total: 0 });
     const [paginaActual, setPaginaActual] = useState(1);
     const [medicionesPorPagina] = useState(24);
-    // eslint-disable-next-line no-unused-vars
-    const [tablaHeight, setTablaHeight] = useState(500);
-    // eslint-disable-next-line no-unused-vars
-    const [tablaWeight, setTablaWeight] = useState(1200);
-    const columnas = ["idmedicion","fecha","hora" ,"Variables", "Equipo", "Tiempo de Lectura"];
+    const [filtro, setFiltro] = useState('');
     const [variablesMedicion, setVariablesMedicion] = useState('');
     const [medicionSeleccionada, setMedicionSeleccionada] = useState(null);
     const [equipoSeleccionado, setEquipoSeleccionado] = useState(null);
-    const [filtro, setFiltro] = useState('');
 
     useEffect(() => {
         const obtenerMediciones = async () => {
@@ -29,7 +24,7 @@ const TablaMediciones = () => {
         };
 
         obtenerMediciones();
-    }, [paginaActual, filtro]);
+    }, [paginaActual, filtro, medicionesPorPagina]);
 
     useEffect(() => {
         const obtenerConteo = async () => {
@@ -47,15 +42,24 @@ const TablaMediciones = () => {
     const handleVerMas = async (idMedicion, equipos) => {
         try {
             const response = await axios.get(`http://localhost:8081/variables/${idMedicion}`);
+            // eslint-disable-next-line no-unused-vars
             const variables = response.data.variables ? response.data.variables.join(';') : '';
-            setVariablesMedicion(variables);
-
+    
+            if (equipos === '5PAV6' || equipos === 'SAV1') {
+                setVariablesMedicion('acuvim');
+            } else if (equipos === '5PAR4' || equipos === '5PAR5' || equipos === 'SAR2' || equipos === 'SAR3') {
+                setVariablesMedicion('acurev');
+            } else if (equipos === '4PEV7') {
+                setVariablesMedicion('ev390');
+            }
+    
             setMedicionSeleccionada(idMedicion);
             setEquipoSeleccionado(equipos);
         } catch (error) {
             console.error('Error al obtener las variables de la medición:', error);
         }
     };
+    
 
     const handleFiltroChange = (e) => {
         setFiltro(e.target.value);
@@ -64,7 +68,7 @@ const TablaMediciones = () => {
     return (
         <div className='flex flex-col items-center pb-12 pt-2'>
             {/* Filtro y botón de búsqueda */}
-            <div className="flex flex-row items-center justify-beetwen">
+            <div className="flex flex-row items-center justify-between">
                 <div className='flex font-mono italic text-lime-500 items-start mr-96 pr-24'>Tabla de mediciones</div>
                 <input
                     type="text"
@@ -78,18 +82,18 @@ const TablaMediciones = () => {
                     className="ml-2 px-2 py-1 bg-lime-200 text-md text-lime-800 hover:bg-lime-300 rounded-lg flex items-center"
                 >
                     <span className="mr-2 text-lime-700">
-                    <Icon icon="octicon:filter-16" width="16" height="16"  />
+                        <Icon icon="octicon:filter-16" width="16" height="16" />
                     </span>
                     Buscar
                 </button>
             </div>
 
             {/* Tabla de mediciones */}
-            <div className="m-4 overflow-x-auto rounded-lg border border-lime-700" style={{ height: tablaHeight, width: tablaWeight, overflowY: 'scroll' }}>
+            <div className="m-4 overflow-x-auto rounded-lg border border-lime-700" style={{ height: '500px', width: '1200px', overflowY: 'scroll' }}>
                 <table className="min-w-full divide-y divide-gray-200 text-sm">
                     <thead className="bg-lime-200">
                         <tr>
-                            {columnas.map((columna) => (
+                            {["idmedicion", "fecha", "hora", "Variables", "Equipo", "Tiempo de Lectura"].map((columna) => (
                                 <th key={columna} className="px-6 py-3 text-center text-xs font-medium text-black uppercase tracking-wider">
                                     {columna}
                                 </th>
@@ -97,20 +101,18 @@ const TablaMediciones = () => {
                         </tr>
                     </thead>
                     <tbody className="bg-white divide-y font-mono divide-gray-200 text-center">
-                        {mediciones.map((medicion) => {
-                            return (
-                                <tr key={medicion.idmedicion} className="hover:bg-gray-100">
-                                    <td className="px-6 whitespace-nowrap border">{medicion.idmedicion}</td>
-                                    <td className="px-6 whitespace-nowrap border">{medicion.fecha}</td>
-                                    <td className="px-6 whitespace-nowrap border">{medicion.hora}</td>
-                                    <td className="px-6 flex justify-center hover:underline hover:text-black items-center font-thin text-lime-700">
-                                        <button onClick={() => handleVerMas(medicion.idmedicion, medicion.equipos_idequipo)}>↻ Ver más</button>
-                                    </td>
-                                    <td className="px-6 whitespace-nowrap border">{medicion.equipos_idequipo}</td>
-                                    <td className="px-6 whitespace-nowrap border">{medicion.tiempoLectura}</td>
-                                </tr>
-                            );
-                        })}
+                        {mediciones.map((medicion) => (
+                            <tr key={medicion.idmedicion} className="hover:bg-gray-100">
+                                <td className="px-6 whitespace-nowrap border">{medicion.idmedicion}</td>
+                                <td className="px-6 whitespace-nowrap border">{medicion.fecha}</td>
+                                <td className="px-6 whitespace-nowrap border">{medicion.hora}</td>
+                                <td className="px-6 flex justify-center hover:underline hover:text-black items-center font-thin text-lime-700">
+                                    <button onClick={() => handleVerMas(medicion.idmedicion, medicion.equipos_idequipo)}>↻ Ver más</button>
+                                </td>
+                                <td className="px-6 whitespace-nowrap border">{medicion.equipos_idequipo}</td>
+                                <td className="px-6 whitespace-nowrap border">{medicion.tiempoLectura}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
                 <div className="flex justify-between mt-4 px-2 pb-4">
--- frontend/src/components/datos/TablaVariables.jsx
+++ frontend/src/components/datos/TablaVariables.jsx
@@ -1,37 +1,78 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-
-// eslint-disable-next-line react/prop-types
 const TablaVariables = ({ variablesMedicion }) => {
+    const [variables, setVariables] = useState([]);
+    const [variablesValues, setVariablesValues] = useState([]);
+
+    useEffect(() => {
+        const fetchVariablesAndValues = async () => {
+            if (variablesMedicion && variablesMedicion !== '') {
+                try {
+                    const response1 = await fetch(`/values/${variablesMedicion}.json`);
+                    const data1 = await response1.json();
+                    setVariables(data1);
+
+                    const response2 = await fetch(`http://localhost:8081/variables/${variablesMedicion}`);
+                    const data2 = await response2.json();
+                    if (data2 && data2.variables) {
+                        setVariablesValues(data2.variables);
+                    } else {
+                        setVariablesValues(new Array(data1.length).fill(0));
+                    }
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    setVariables([]);
+                    setVariablesValues([]);
+                }
+            }
+        };
+
+        fetchVariablesAndValues();
+    }, [variablesMedicion]);
+
     return (
         <div className="flex mt-6">
-            <table className="min-w-full divide-y border border-collapse border-lime-500  divide-gray-200">
+            <table className="min-w-full divide-y border border-collapse border-lime-500 divide-gray-200">
                 <thead className="bg-gray-50 border border-lime-500 rounded-lg">
                     <tr>
                         <th scope="col" className="px-6 py-3 text-left text-xs text-lime-700 tracking-wider">
                             Nombre Variable
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs  text-lime-700 tracking-wider">
+                        <th scope="col" className="px-6 py-3 text-left text-xs text-lime-700 tracking-wider">
                             Valor
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs  text-lime-700 tracking-wider">
+                        <th scope="col" className="px-6 py-3 text-left text-xs text-lime-700 tracking-wider">
+                            # Registro
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs text-lime-700 tracking-wider">
                             Conversión
                         </th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs  text-lime-700 tracking-wider">
+                        <th scope="col" className="px-6 py-3 text-left text-xs text-lime-700 tracking-wider">
                             Tipo
                         </th>
                     </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                    {variablesMedicion && variablesMedicion.split(';').map((variable, index) => (
+                    {variables.map((variable, index) => (
                         <tr key={index}>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{`Variable ${index + 1}`}</td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{variable}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{variable['Nombre variable']}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                {variablesValues[index] !== undefined ? variablesValues[index] : 'N/A'}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">{variable['# registro']}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{variable['conversion']}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{variable['tipo']}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
         </div>
     );
+};
+
+TablaVariables.propTypes = {
+    variablesMedicion: PropTypes.string.isRequired,
 };
 
 export default TablaVariables;
